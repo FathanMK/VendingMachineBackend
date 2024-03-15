2d4f7acfdedbeb73f23c924040fbfd0f5eedae82
Fathan MK <emailfathanmk@gmail.com>
2024-03-15 14:47:27 +0700

    fix: remove custom routes

--- api/server.js
+++ api/server.js
@@ -17,17 +17,9 @@ const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
-  })
-);
 server.use(router);
 server.listen(8008, () => {
   console.log("JSON Server is running");
 });
 
-// Export the Server API
 module.exports = server;
